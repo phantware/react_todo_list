5a116ac3773046f9b9911ba7fa9d1b60b0450a40
phantware <phantmoney2011@gmail.com>
2020-06-12 13:28:20 +0100

    link fron frontend to backend successfully created

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
 import React, { Fragment } from "react";
 import "./App.css";
 
+//Components
+import InputTodo from "./components/InputTodo";
+
 function App() {
-  return <Fragment></Fragment>;
+  return (
+    <Fragment>
+      <div className="container">
+        <InputTodo></InputTodo>
+      </div>
+    </Fragment>
+  );
 }
 
 export default App;
